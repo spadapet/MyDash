6eb9783ee2df304a6a21341ee1a6b9963e764f6e
Peter Spada <peterspa@microsoft.com>
2023-03-10 16:33:12 -0800

    First attempt login

--- MyDash.Data/Utility/AuthenticationUtility.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyDash.Data.Utility;
-
-public static class AuthenticationUtility
-{
-    // Cross platform Token Cache:
-    // https://github.com/AzureAD/microsoft-authentication-extensions-for-dotnet/wiki/Cross-platform-Token-Cache
-
-
-}
--- MyDash/LoginPage.xaml.cs
+++ MyDash/LoginPage.xaml.cs
@@ -2,14 +2,16 @@
 using MyDash.Data.Model;
 using MyDash.Data.Utility;
 using System;
-using System.IO;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Threading;
 
 namespace MyDash;
 
 public partial class LoginPage : ContentPage
 {
     public LoginModel Model { get; }
+    private CancellationTokenSource cancellationTokenSource;
 
     public LoginPage()
     {
@@ -19,15 +21,18 @@ public partial class LoginPage : ContentPage
 
     private void OnLoaded(object sender, EventArgs args)
     {
+        this.cancellationTokenSource = new CancellationTokenSource();
+
         TaskUtility.FileAndForget(async () =>
         {
-            await Task.Delay(3000);
-            this.Model.SetError(new FileNotFoundException("Missing some file", "foo.bar"));
+            AuthenticationHeaderValue foo = await AdoAuthenticationUtility.GetAuthenticationHeaderValueAsync(this.cancellationTokenSource.Token);
+            Debug.Assert(foo != null);
         });
     }
 
     private void OnUnloaded(object sender, EventArgs args)
     {
-
+        this.cancellationTokenSource?.Cancel();
+        this.cancellationTokenSource = null;
     }
 }
